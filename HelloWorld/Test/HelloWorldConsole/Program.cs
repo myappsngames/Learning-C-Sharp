90d54f1bd74abe51134dcef1f5b70cd2192087c5
myappsngames <62448812+myappsngames@users.noreply.github.com>
2023-09-14 17:26:59 -0700

    Added Hello World Console program

---
+++ HelloWorld/Test/HelloWorldConsole/Program.cs
@@ -0,0 +1,11 @@
+﻿// Uncomment this if you're using .NET 7.0 Top-level statements
+// Console.WriteLine("Hello World!");
+
+// For beginner's I recommend the class level approach below
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World!");
+    }
+}
